85a9a16d6d35a46c0d3300e995277a0656754005
ranjan_rs <ranjan@rugvedsoft.com>
2022-02-04 21:08:54 +0530

    Updated the output text

--- src/main/java/com/rugvedsoft/bcryptutil/Main.java
+++ src/main/java/com/rugvedsoft/bcryptutil/Main.java
@@ -16,26 +16,49 @@ public class Main implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("\n\n=======================================");
+        System.out.println("\n\n=========================\n\n");
         if (args.length < 1) {
-            System.err.println("Provide text to encrypt");
+            System.err.println("\n\nUsage: java -jar bcryptutil-1.0-exec.jar <text-to-encode> <encoder-strength-between-4-and-31> [password-salt]\n\n");
         } else {
-            String toEncode = args[0];
-            String passwordSalt = null;
+            boolean proceed = true;
+            Integer encoderStrength = 12;
             if (args.length > 1) {
-                passwordSalt = args[1];
+                try {
+                    encoderStrength = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    System.err.println("Password Encoder strength must be an integer");
+                    proceed = false;
+                    System.out.println("\n\n");
+                }
+                if (encoderStrength < 4 || encoderStrength > 31) {
+                    System.err.println("Password Encoder strength must be between 4 and 31");
+                    proceed = false;
+                    System.out.println("\n\n");
+                }
             }
-            BCryptPasswordEncoder encoder = null;
-            if (passwordSalt != null) {
-                encoder = new BCryptPasswordEncoder(12, new SecureRandom(passwordSalt.getBytes()));
-            } else {
-                encoder = new BCryptPasswordEncoder(12);
+            if (proceed) {
+                String passwordSalt = null;
+                if (args.length > 2) {
+                    passwordSalt = args[2];
+                }
+                String toEncode = args[0];
+                BCryptPasswordEncoder encoder = null;
+                if (passwordSalt != null) {
+                    encoder = new BCryptPasswordEncoder(encoderStrength, new SecureRandom(passwordSalt.getBytes()));
+                } else {
+                    encoder = new BCryptPasswordEncoder(encoderStrength);
+                }
+                System.out.println("Text to encode: \t" + toEncode);
+                System.out.println("Encoder strength: \t" + encoderStrength);
+                if (passwordSalt != null) {
+                    System.out.println("Random Password Salt: \t" + passwordSalt);
+                }
+                System.out.println("\nEncoded text: [\n");
+                System.out.println(encoder.encode(toEncode));
+                System.out.println("\n]");
             }
-            System.out.println("Text to encode: " + toEncode);
-            System.out.println("Encoded text: [");
-            System.out.println(encoder.encode(toEncode));
-            System.out.println("]");
+
         }
-        System.out.println("\n\n=======================================\n\n");
+        System.out.println("\n\n=========================\n\n");
     }
 }
